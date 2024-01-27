0bb307660952313824d8ced515727ac804059703
Alexander Ciesielski <alexander@ciesielski.co>
2024-01-27 06:34:10 +0100

    chore: better untilDestroyed DX

--- libs/angular-utils/src/lifecycle/until-destroyed.ts
+++ libs/angular-utils/src/lifecycle/until-destroyed.ts
@@ -1,9 +1,8 @@
-import { Observable, takeUntil } from 'rxjs';
-import { RxOnDestroy } from './rx-on-destroy';
+import { Observable, isObservable, takeUntil } from 'rxjs';
 
-export function untilDestroyed<T extends RxOnDestroy>(instance: T) {
+export function untilDestroyed<T extends object>(instance: T) {
   return <U>(source: Observable<U>) => {
-    if (!instance.ngOnDestroy$) {
+    if (!('ngOnDestroy$' in instance) || !isObservable(instance['ngOnDestroy$'])) {
       throw new Error(`${instance.constructor.name} is using untilDestroyed but doesn't implement RxOnDestroy`);
     }
 
